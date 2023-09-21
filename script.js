748476ac4a95126c39aa1bc652fc84b4af438796
HugoLeonardo-hub <hugoperes21@gmail.com>
2023-09-20 22:53:31 -0300

    .

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
 
-function ativaletra(elemento){
-    const arrtexto = elemento.innerHTML.split('');
-    elemento.innerHTML = '';
-    arrtexto.forEach((letra, i) =>{
-        setTimeout(()=>{
-            elemento.innerHTML += letra;
-        },75 *i);
-    });
-}
 
-const titulo = document.querySelector('.digitando');
+function escrevendoLetra(){
+    function ativaLetra(elemento){
+        const arrTexto = elemento.innerHTML.split('');
+        elemento.innerHTML  = '';
+        arrTexto.forEach((letra, i)=>{
+            setTimeout(()=>{
+                elemento.innerHTML += letra; 
+            }, 75 * i);
+        });
+    }
+
+    const titulo = document.querySelector('.digitando');
+    ativaLetra(titulo);
+    
+} 
+escrevendoLetra();
 
 
-ativaletra(titulo);
+const ativaMenu = document.querySelector('.fa-bars')
 
+ativaMenu.addEventListener('click', ()=>{
+    ativaMenu.classList.toggle('fa-x')
+})
 
