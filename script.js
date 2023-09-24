c18fd08f77c7571eefc2acc7cfabc4a6ecbd6bcc
HugoLeonardo-hub <hugoperes21@gmail.com>
2023-09-23 23:16:09 -0300

    animação de menu e efeito de hover nas habilidades

--- script.js
+++ script.js
@@ -19,8 +19,10 @@ escrevendoLetra();
 
 
 const ativaMenu = document.querySelector('.fa-bars')
+const navMenu = document.querySelector('header .navegacao-primaria')
 
 ativaMenu.addEventListener('click', ()=>{
     ativaMenu.classList.toggle('fa-x')
+    navMenu.classList.toggle('ativado')
 })
 
