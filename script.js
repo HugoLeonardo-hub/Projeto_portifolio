8b1c3d74ac5e646627b5b04112542a37b3c16a5d
HugoLeonardo-hub <hugoperes21@gmail.com>
2023-10-06 15:58:25 -0300

    Seções experience e education
    seções finalizadas e seção my skills iniciada

--- script.js
+++ script.js
@@ -18,11 +18,64 @@ function escrevendoLetra(){
 escrevendoLetra();
 
 
-const ativaMenu = document.querySelector('.fa-bars')
-const navMenu = document.querySelector('header .navegacao-primaria')
+function menuMobile(){
 
-ativaMenu.addEventListener('click', ()=>{
+    const ativaMenu = document.querySelector('.fa-bars')
+    const navMenu = document.querySelector('header .navegacao-primaria')
+
+    ativaMenu.addEventListener('click', ()=>{
     ativaMenu.classList.toggle('fa-x')
     navMenu.classList.toggle('ativado')
-})
+    });
+}
 
+menuMobile();
+
+
+function aboutMe(){
+    const divExperience = document.querySelectorAll('.experience_content div'); 
+    const liExperience = document.querySelectorAll('.experience_content ul li'); 
+    const divEducation = document.querySelectorAll('.education_content div'); 
+    const liEducation = document.querySelectorAll('.education_content ul li'); 
+
+    divEducation[0].classList.add('active');
+    liEducation[0].classList.add('active');
+    divExperience[0].classList.add('active');
+    liExperience[0].classList.add('active');
+    
+    function slideShow(index){
+        divExperience.forEach((div)=>{
+            div.classList.remove('active')
+        });
+        liExperience.forEach((buttom)=>{
+            buttom.classList.remove('active');
+        })
+        divExperience[index].classList.add('active');
+        liExperience[index].classList.add('active');
+    }
+    
+    function slideShow2(index){
+        divEducation.forEach((div)=>{
+            div.classList.remove('active')
+        });
+        liEducation.forEach((buttom)=>{
+            buttom.classList.remove('active');
+        })
+        divEducation[index].classList.add('active');
+        liEducation[index].classList.add('active');
+    }
+    
+    liExperience.forEach((event, index) => {
+        event.addEventListener('click', () =>{
+            slideShow(index);
+        })
+    });
+    
+    liEducation.forEach((event, index) => {
+        event.addEventListener('click', () =>{
+            slideShow2(index);
+        })
+    });
+}
+
+aboutMe();
